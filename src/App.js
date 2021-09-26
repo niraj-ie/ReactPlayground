e4e5b349d9518ca1b38055cd9ac3b26ba67dfc41
niraj <niraj.lama@ie.com.au>
2021-09-27 09:36:43 +1000

    Simple todo list

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+import Form from "./Form";
+import Table from "./Table";
+
+class App extends Component {
+  state = {
+    characters: [],
+  };
+
+  removeCharacter = (index) => {
+    const { characters } = this.state;
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      }),
+    });
+  };
+
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] });
+  };
+
+  render() {
+    const { characters } = this.state;
+    return (
+      <div className="container">
+        <h1>React Learning</h1>
+        <p>
+          To get started add a character with a name and a job to populate
+          table.
+        </p>
+        <Table
+          characterData={characters}
+          removeCharacter={this.removeCharacter}
+        />
+        <Form handleSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
+}
+
+export default App;
